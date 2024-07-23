e79456117c8def792c36f6b36038ee0cf54b5d5a
Danil Schumin <danil80sch@gmail.com>
2024-07-23 10:31:55 +0200

    worked on flakes and dev environment for Development
    Signed-off-by: Danil Schumin <danil80sch@gmail.com>

--- server/flake.nix
+++ server/flake.nix
@@ -3,25 +3,32 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    nix-minecraft.url = "github:Infinidoge/nix-minecraft";
   };
 
-  outputs = { self , nixpkgs, ... }:
+  outputs = { self , nixpkgs, nix-minecraft, inputs,  ... }:
     let pkgs = import nixpkgs { system = "x86_64-linux"; config.allowUnfree = true; };
     in {
       devShells.x86_64-linux.minecraft-dev = pkgs.mkShell {
             packages = with pkgs; [
-	      papermc
-              minecraft-server
-              lunar-client
-              jdk17
-              jre17_minimal
-
+                prismlauncher
             ];
 
-            # TODO: Somehow make lunar stop with all these logs >:(
+            imports = [ inputs.nix-minecraft.nixosModules.minecraft-servers ];
+            nixpkgs.overlays = [ nix-minecraft.overlay ];
+            services.minecraft-servers = {
+              enable = true;
+              eula = true;
+
+              servers.dungeons = {
+                enable = true;
+                package = pkgs.vanillaServers.vanilla;
+                };
+            };
+
             shellHook = ''
-                echo eula=true > eula.txt
-                minecraft-server
+                prismlauncher
             '';
         };
       };
